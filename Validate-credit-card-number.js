bb9d28ffdbb81091067e00c1caad9e12e991cc71
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-12 14:29:00 -0800

    Update Validate-credit-card-number.js

--- Validate-credit-card-number.js
+++ Validate-credit-card-number.js
@@ -39,3 +39,11 @@ function validate(n) {
 }
 
 // or
+
+function validate(n){
+  return [...String(n)]
+    .reverse()
+    .map((val, index) => index % 2 ? val * 2 : Number(val))
+    .map((val, index) => val > 9 ? val - 9 : val)
+    .reduce((prev, curr) => prev + curr) % 10 ? false : true;
+}
