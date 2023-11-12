a6bf387503807fb096a15a10442cad949669c8b8
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-12 14:28:31 -0800

    Update Validate-credit-card-number.js

--- Validate-credit-card-number.js
+++ Validate-credit-card-number.js
@@ -28,3 +28,12 @@ or:
 
 
 // Solution
+
+function validate(n) {
+  n = n.toString().split('').map(Number).reverse();
+  return n.reduce(function (sum, digit, index) {
+    if (index & 1) digit <<= 1;
+    if (digit > 9) digit -= 9;
+    return sum + digit;
+  }, 0) % 10 == 0;
+}
